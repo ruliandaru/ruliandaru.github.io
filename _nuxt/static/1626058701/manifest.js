71cd089ac51ec2693f10522085cb0cc3b8b0b576
Ruli Andaru <ruliandaru@ugm.ac.id>
2021-07-12 10:05:04 +0700

    change link to publication

---
+++ _nuxt/static/1626058701/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fpublication","\u002Fresearch","\u002Fsoftware"]})
---
+++ _nuxt/static/1626058701/publication/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626058701",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpublication",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6a833872"}},globalRefs:{}}}(null));
